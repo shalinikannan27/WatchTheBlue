b11dfe56958b010b963717d66176eaf43327b7ac
Khanak Juttiyavar <khanakbjuttiyavar@gmail.com>
2026-05-12 15:19:34 +0530

    Add files via upload
    Species tolerance database

---
+++ species_data.py
@@ -0,0 +1,27 @@
+species_data = {
+    "Olive Ridley Turtle": {
+        "temperature_range": (24, 30),   # °C
+        "oxygen_range": (4, 8),          # mg/L
+        "salinity_range": (30, 36)       # PSU
+    },
+    "Whale Shark": {
+        "temperature_range": (21, 30),
+        "oxygen_range": (3.5, 7),
+        "salinity_range": (32, 35)
+    },
+    "Spinner Dolphin": {
+        "temperature_range": (24, 29),
+        "oxygen_range": (5, 8),
+        "salinity_range": (33, 36)
+    },
+    "Humpback Whale": {
+        "temperature_range": (10, 28),
+        "oxygen_range": (5, 9),
+        "salinity_range": (32, 35)
+    },
+    "Dugong": {
+        "temperature_range": (23, 30),
+        "oxygen_range": (4, 7),
+        "salinity_range": (28, 35)
+    }
+}
